5b682f8d8d3bd761a169031b808f9b62d21abb53
Fayyad M Madani <fayyadmadani.11@gmail.com>
2026-06-08 10:57:24 +0700

    feat: sesi-14 Product Display Integration

---
+++ sesi-14/tugas-laravel/resources/views/components/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+    <div class="container">
+        <a class="navbar-brand" href="/">Toko Fayyad</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="/products">Katalog Produk</a></li>
+                <li class="nav-item"><a class="nav-link" href="/about">Tentang Kami</a></li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/cart">
+                        Keranjang
+                        @if(session('cart'))
+                            <span class="badge bg-danger">{{ count(session('cart')) }}</span>
+                        @endif
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
